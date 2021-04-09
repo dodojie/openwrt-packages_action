257f3e8e0c794b012c5586a2a1003d6ffd3c9bff
dodojie <dodo.jie@163.com>
2021-04-09 19:23:06 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -6,8 +6,8 @@ svn co https://github.com/fw876/helloworld/trunk ./luci-app-lean-ssr
 svn co https://github.com/jerrykuku/lua-maxminddb/trunk ./luci-app-vssr/lua-maxminddb
 svn co https://github.com/jerrykuku/luci-app-vssr/trunk ./luci-app-vssr/luci-app-vssr
 rm -rf .svn
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns ./luci-app-smartdns
-svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns ./luci-app-smartdns
+svn co https://github.com/pymumu/luci-app-smartdns//branches/lede ./luci-app-smartdns/luci-app-smartdns
+svn co https://github.com/pymumu/openwrt-smartdns/trunk ./luci-app-smartdns/openwrt-smartdns
 svn co https://github.com/garypang13/luci-theme-edge/branches/18.06 ./luci-theme-edge
 svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
 svn co https://github.com/destan19/OpenAppFilter/trunk ./luci-app-oaf
