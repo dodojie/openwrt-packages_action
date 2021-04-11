e4bfdcea711b748d9afdf1e359f7dc97959efcd6
dodojie <dodo.jie@163.com>
2021-04-11 22:46:24 +0800

    Update openwrt_21.02.sh

--- openwrt_21.02.sh
+++ openwrt_21.02.sh
@@ -5,6 +5,7 @@
 
 svn co https://github.com/jerrykuku/luci-theme-argon/trunk ./luci-theme-argon
 svn co https://github.com/jerrykuku/luci-app-argon-config/trunk ./luci-theme-argon/luci-app-argon-config
+rm -rf ./luci-theme-argon/luci-app-argon-config/.svn
 svn co https://github.com/jerrykuku/luci-app-jd-dailybonus/trunk ./luci-app-jd-dailybonus
 svn co https://github.com/jerrykuku/luci-app-ttnode/trunk ./luci-app-ttnode
 svn co https://github.com/jerrykuku/lua-maxminddb/trunk ./luci-app-vssr/lua-maxminddb
@@ -13,6 +14,8 @@ svn co https://github.com/jerrykuku/luci-app-vssr/trunk ./luci-app-vssr/luci-app
 svn co https://github.com/KFERMercer/luci-app-baidupcs-web/trunk ./luci-app-baidupcs-web/luci-app-baidupcs-web
 svn co https://github.com/KFERMercer/openwrt-baidupcs-web/trunk ./luci-app-baidupcs-web/openwrt-baidupcs-web
 svn co https://github.com/KFERMercer/luci-app-tcpdump/trunk ./luci-app-tcpdump
+rm -rf ./luci-app-baidupcs-web/luci-app-baidupcs-web/.svn
+rm -rf ./luci-app-baidupcs-web/openwrt-baidupcs-web/.svn
 
 svn co https://github.com/lisaac/luci-app-diskman/trunk ./luci-app-diskman
 svn co https://github.com/honwen/luci-app-aliddns/trunk ./luci-app-aliddns
@@ -20,6 +23,8 @@ svn co https://github.com/sensec/ddns-scripts_aliyun/trunk ./ddns-scripts_aliyun
 svn co https://github.com/garypang13/openwrt-packages/trunk/luci-app-softethervpn5 ./luci-app-softethervpn5/luci-app-softethervpn5
 svn co https://github.com/garypang13/openwrt-packages/trunk/softethervpn5 ./luci-app-softethervpn5/softethervpn5
 svn co https://github.com/BCYDTZ/luci-app-UUGameAcc/trunk ./luci-app-UUGameAcc
+rm -rf ./luci-app-softethervpn5/luci-app-softethervpn5/.svn
+rm -rf ./luci-app-softethervpn5/softethervpn5/.svn
 
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-adbyby-plus  ./luci-app-adbyby-plus/luci-app-adbyby-plus
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/adbyby  ./luci-app-adbyby-plus/adbyby
@@ -41,6 +46,18 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/tcpping
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/dnsforwarder
+rm -rf ./luci-app-adbyby-plus/luci-app-adbyby-plus5/.svn
+rm -rf ./luci-app-adbyby-plus/adbyby/.svn
+rm -rf ./luci-app-qbittorrent/qBittorrent/.svn
+rm -rf ./luci-app-qbittorrent/luci-app-qbittorrent/.svn
+rm -rf ./luci-app-rclone/luci-app-rclone/.svn
+rm -rf ./luci-app-rclone/rclone-ng/.svn
+rm -rf ./luci-app-rclone/rclone-webui-react/.svn
+rm -rf ./luci-app-rclone/rclone/.svn
+rm -rf ./luci-app-verysync/luci-app-verysync/.svn
+rm -rf ./luci-app-verysync/verysync/.svn
+rm -rf ./luci-app-vlmcsd/luci-app-vlmcsd/.svn
+rm -rf ./luci-app-vlmcsd/vlmcsd/.svn
 
 
 git clone https://github.com/tty228/luci-app-serverchan 
