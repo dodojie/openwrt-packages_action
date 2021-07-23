eb15fb8eaf4cf6da4514fe0088515a3184ee02dd
dodojie <dodo.jie@163.com>
2021-07-23 18:21:53 +0800

    Update openwrt_18.06.sh

--- openwrt_18.06.sh
+++ openwrt_18.06.sh
@@ -8,10 +8,6 @@ svn co https://github.com/jerrykuku/lua-maxminddb/trunk ./luci-app-vssr/lua-maxm
 svn co https://github.com/jerrykuku/luci-app-vssr/trunk ./luci-app-vssr/luci-app-vssr
 rm -rf ./luci-app-vssr/lua-maxminddb/.svn
 rm -rf ./luci-app-vssr/luci-app-vssr/.svn
-svn co https://github.com/pymumu/luci-app-smartdns//branches/lede ./luci-app-smartdns/luci-app-smartdns
-svn co https://github.com/pymumu/openwrt-smartdns/trunk ./luci-app-smartdns/openwrt-smartdns
-rm -rf ./luci-app-smartdns/luci-app-smartdns/.svn
-rm -rf ./luci-app-smartdns/openwrt-smartdns/.svn
 svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
 svn co https://github.com/destan19/OpenAppFilter/trunk ./luci-app-oaf
 svn co https://github.com/riverscn/openwrt-iptvhelper/trunk  ./luci-app-iptvhelper
@@ -21,7 +17,6 @@ svn co https://github.com/project-lede/luci-app-godproxy/trunk ./luci-app-godpro
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk ./luci-app-passwall
 svn co https://github.com/garypang13/luci-app-eqos/trunk ./luci-app-eqos
 svn co https://github.com/Hyy2001X/luci-app-autoupdate/trunk ./luci-app-autoupdate
-svn co https://github.com/NateLol/luci-app-beardropper/trunk ./luci-app-beardropper
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-control-webrestriction
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-control-weburl
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-socat
