6be92e9fa617a2fdadef560942fef60e8116c685
HERLON <dodo.jie@163.com>
2022-12-21 20:00:29 +0800

    Update openwrt_21.02.sh

--- openwrt_21.02.sh
+++ openwrt_21.02.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 svn co https://github.com/jerrykuku/luci-theme-argon/trunk ./luci-theme-argon
-svn co https://github.com/jerrykuku/luci-app-argon-config/trunk ./luci-theme-argon/luci-app-argon-config
+svn co https://github.com/jerrykuku/luci-app-argon-config/trunk ./luci-app-argon-config
 rm -rf ./luci-theme-argon/luci-app-argon-config/.svn
 svn co https://github.com/fw876/helloworld/trunk ./luci-app-ssr-plus
 svn co https://github.com/destan19/OpenAppFilter/trunk ./luci-app-oaf
@@ -12,6 +12,9 @@ svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-control-weburl
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-control-timewol
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-socat
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-timecontrol
+svn co https://github.com/xiaorouji/openwrt-passwall/branches/packages ./openwrt-passwall
+svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci 
+svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 ./luci-app-passwall2
 svn co https://github.com/sirpdboy/luci-app-autotimeset/trunk ./luci-app-autotimeset
 svn co https://github.com/sirpdboy/luci-app-ddns-go/trunk ./luci-app-ddns-go
 svn co https://github.com/sirpdboy/luci-app-advanced/trunk ./luci-app-advanced
@@ -19,10 +22,10 @@ svn co https://github.com/sirpdboy/netspeedtest/trunk ./netspeedtest
 svn co https://github.com/sirpdboy/luci-app-lucky/trunk ./luci-app-lucky
 svn co https://github.com/lisaac/luci-app-diskman/trunk ./luci-app-diskman
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/adbyby ./luci-app-adbyby-plus/adbyby
-svn co https://github.com/coolsnowwolf/packages/trunk/net/vlmcsd ./luci-app-vlmcsd
+svn co https://github.com/coolsnowwolf/packages/trunk/net/vlmcsd ./luci-app-vlmcsd/vlmcsd
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ddns-scripts_aliyun
 svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-adbyby-plus ./luci-app-adbyby-plus/luci-app-adbyby-plus
-svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-vlmcsd
+svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-vlmcsd ./luci-app-vlmcsd/luci-app-vlmcsd
 svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-turboacc
 
 rm -rf ./*/.git
