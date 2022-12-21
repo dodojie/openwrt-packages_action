861c0588ff2704664c48ad2cbab946cabad11a6a
HERLON <dodo.jie@163.com>
2022-12-21 22:47:27 +0800

    Update openwrt_21.02.sh

--- openwrt_21.02.sh
+++ openwrt_21.02.sh
@@ -11,6 +11,7 @@ svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-control-weburl
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-control-timewol
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-socat
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-timecontrol
+svn co https://github.com/Lienol/openwrt/trunk/package/kernel/shortcut-fe ./shortcut-fe
 git clone --depth 1 -b packages https://github.com/xiaorouji/openwrt-passwall ./openwrt-passwall
 svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall ./luci-app-passwall
 svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 ./luci-app-passwall2
@@ -25,7 +26,7 @@ svn co https://github.com/Lienol/openwrt-package/branches/other/lean/luci-app-ad
 svn co https://github.com/Lienol/openwrt-package/branches/other/lean/luci-app-vlmcsd ./luci-app-vlmcsd/luci-app-vlmcsd
 svn co https://github.com/Lienol/openwrt-package/branches/other/lean/luci-app-turboacc ./luci-app-turboacc
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ddns-scripts_aliyun
-svn co https://github.com/Lienol/openwrt/trunk/package/kernel/shortcut-fe
+
 
 
 rm -rf ./*/.git
