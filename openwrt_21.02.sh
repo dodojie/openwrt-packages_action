42c7a82250ee77180ea78ff12777363c048e91f2
HERLON <dodo.jie@163.com>
2022-12-26 19:28:17 +0800

    Update openwrt_21.02.sh

--- openwrt_21.02.sh
+++ openwrt_21.02.sh
@@ -28,10 +28,10 @@ svn co https://github.com/sirpdboy/luci-app-lucky/trunk ./luci-app-lucky
 # ln -s zh-cn ./luci-app-control-webrestriction/po/zh_Hans
 # ln -s zh-cn ./luci-app-control-weburl/po/zh_Hans
 # ln -s zh-cn ./luci-app-control-timewol/po/zh_Hans
-# rm -rf ./luci-app-ddns-go/luci-app-ddns-go/po/zh_Hans
-# ln -s zh-cn ./luci-app-ddns-go/luci-app-ddns-go/po/zh_Hans
-# rm -rf ./luci-app-lucky/luci-app-lucky/po/zh_Hans
-# ln -s zh-cn ./luci-app-lucky/luci-app-lucky/po/zh_Hans
+rm -rf ./luci-app-ddns-go/luci-app-ddns-go/po/zh_Hans
+ln -s zh-cn ./luci-app-ddns-go/luci-app-ddns-go/po/zh_Hans
+rm -rf ./luci-app-lucky/luci-app-lucky/po/zh_Hans
+ln -s zh-cn ./luci-app-lucky/luci-app-lucky/po/zh_Hans
 # ln -s zh-cn ./luci-app-tcpdump/po/zh_Hans
 # ln -s zh-cn ./luci-app-timecontrol/po/zh_Hans
 # ln -s zh-cn ./luci-app-vlmcsd/luci-app-vlmcsd/po/zh_Hans
