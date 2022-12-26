998d2d8f1ed5607160a1c8095f1b13661eab752d
HERLON <dodo.jie@163.com>
2022-12-26 16:55:57 +0800

    Update and rename openwrt_22.03.sh to openwrt_21.02.sh

---
+++ openwrt_21.02.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# svn co https://github.com/jerrykuku/luci-theme-argon/trunk ./luci-theme-argon
+# svn co https://github.com/jerrykuku/luci-app-argon-config/trunk ./luci-app-argon-config
+svn co https://github.com/fw876/helloworld/trunk ./luci-app-ssr-plus
+svn co https://github.com/destan19/OpenAppFilter/trunk ./luci-app-oaf
+svn co https://github.com/ilxp/luci-app-ikoolproxy/trunk ./luci-app-ikoolproxy
+# svn co https://github.com/Lienol/openwrt-package/branches/other/luci-app-tcpdump 
+# svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-control-webrestriction
+# svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-control-weburl
+# svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-control-timewol
+# svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-socat
+# svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-timecontrol
+git clone --depth 1 -b packages https://github.com/xiaorouji/openwrt-passwall ./openwrt-passwall
+svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall ./luci-app-passwall
+svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 ./luci-app-passwall2
+svn co https://github.com/sirpdboy/luci-app-autotimeset/trunk ./luci-app-autotimeset
+svn co https://github.com/sirpdboy/luci-app-ddns-go/trunk ./luci-app-ddns-go
+svn co https://github.com/sirpdboy/luci-app-advanced/trunk ./luci-app-advanced
+svn co https://github.com/sirpdboy/luci-app-lucky/trunk ./luci-app-lucky
+# svn co https://github.com/lisaac/luci-app-diskman/trunk ./luci-app-diskman
+# svn co https://github.com/Lienol/openwrt-package/branches/other/lean/adbyby ./luci-app-adbyby-plus/adbyby
+# svn co https://github.com/Lienol/openwrt-package/branches/other/lean/vlmcsd ./luci-app-vlmcsd/vlmcsd
+# svn co https://github.com/Lienol/openwrt-package/branches/other/lean/luci-app-adbyby-plus ./luci-app-adbyby-plus/luci-app-adbyby-plus
+# svn co https://github.com/Lienol/openwrt-package/branches/other/lean/luci-app-vlmcsd ./luci-app-vlmcsd/luci-app-vlmcsd
+
+# ln -s zh-cn ./luci-app-adbyby-plus/luci-app-adbyby-plus/po/zh_Hans
+# ln -s zh-cn ./luci-app-control-webrestriction/po/zh_Hans
+# ln -s zh-cn ./luci-app-control-weburl/po/zh_Hans
+# ln -s zh-cn ./luci-app-control-timewol/po/zh_Hans
+# rm -rf ./luci-app-ddns-go/luci-app-ddns-go/po/zh_Hans
+# ln -s zh-cn ./luci-app-ddns-go/luci-app-ddns-go/po/zh_Hans
+# rm -rf ./luci-app-lucky/luci-app-lucky/po/zh_Hans
+# ln -s zh-cn ./luci-app-lucky/luci-app-lucky/po/zh_Hans
+# ln -s zh-cn ./luci-app-tcpdump/po/zh_Hans
+# ln -s zh-cn ./luci-app-timecontrol/po/zh_Hans
+# ln -s zh-cn ./luci-app-vlmcsd/luci-app-vlmcsd/po/zh_Hans
+# # sed -i '1i\msgid ""\nmsgstr "Content-Type: text/plain; charset=UTF-8"\n' ./luci-app-timecontrol/po/zh-cn/timecontrol.po
+# sed -i '8s/LUCI_TITLE:=LuCI support for Time Control/PKG_NAME:=luci-app-timecontrol/' ./luci-app-timecontrol/Makefile
+# sed -i '8s/LUCI_TITLE:=LuCI support for Adbyby/PKG_NAME:=luci-app-adbyby-plus/' ./luci-app-adbyby-plus/luci-app-adbyby-plus/Makefile
+# sed -i '9s/LUCI_TITLE:=LuCI support for KMS/PKG_NAME:=luci-app-vlmcsd/' ./luci-app-vlmcsd/luci-app-vlmcsd/Makefile
+# sed -i '10s/LUCI_TITLE:=LuCI support for tcpdump/PKG_NAME:=luci-app-tcpdump/' ./luci-app-tcpdump/Makefile
+# sed -i '13d' ./luci-app-tcpdump/Makefile
+# sed -i '8s/LUCI_TITLE:=LuCI support for Webrestriction From Koolshare/PKG_NAME:=luci-app-control-webrestriction/' ./luci-app-control-webrestriction/Makefile
+# sed -i '8s/LUCI_TITLE:=LuCI support for Timewol From Koolshare/PKG_NAME:=luci-app-control-timewol/' ./luci-app-control-timewol/Makefile
+# sed -i '8s/LUCI_TITLE:=LuCI support for Weburl From Koolshare/PKG_NAME:=luci-app-control-weburl/' ./luci-app-control-weburl/Makefile
+
+rm -rf ./*/.git
+rm -rf ./*/.github
+rm -rf ./*/*/.github
+rm -rf ./*/.svn 
+rm -rf ./*/*/.svn 
+rm -f .gitattributes .gitignore
+exit 0
